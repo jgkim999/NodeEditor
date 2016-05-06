00bd1f03ebc57b41e8d3f4feb140e99b81af40c3
jgkim <jgkim@flintgames.co.kr>
2016-05-06 18:57:26 +0900

    Node editor example.

---
+++ Assets/Editor/BaseInputNode.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class BaseInputNode : BaseNode
+{
+    public virtual string getResult()
+    {
+        return "None";
+    }
+
+    public override void DrawCurves()
+    {
+    }
+}
---
+++ Assets/Editor/NodeEditor.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+
+public class NodeEditor : EditorWindow
+{
+    private List<BaseNode> windows = new List<BaseNode>();
+    private Vector2 mousePos;
+    private BaseNode selectednode;
+    private bool makeTransitionMode = false;
+
+    [MenuItem("Windows/Node Editor")]
+    static void ShowEditor()
+    {
+        //NodeEditor editor = EditorWindow.GetWindow<>();
+    }
+
+    public static void DrawNodeCurve(Rect start, Rect end)
+    {
+
+    }
+}
